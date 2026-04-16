15bb9a61ba13a7dc414b5f4ffe16d33c79e39ed1
s1080823 <s1080823@gm.pu.edu.tw>
2026-04-16 11:04:44 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -5,12 +5,9 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# 判斷是在 Vercel 還是本地
 if os.path.exists('serviceAccountKey.json'):
-    # 本地環境：讀取檔案
     cred = credentials.Certificate('serviceAccountKey.json')
 else:
-    # 雲端環境：從環境變數讀取 JSON 字串
     firebase_config = os.getenv('FIREBASE_CONFIG')
     cred_dict = json.loads(firebase_config)
     cred = credentials.Certificate(cred_dict)
