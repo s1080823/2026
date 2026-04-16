2c9c9a098066d328435240c7ba25e4006e3069f8
s1080823 <s1080823@gm.pu.edu.tw>
2026-04-16 12:24:07 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -32,7 +32,7 @@ def index():
     link += "<br><a href=/read>讀取Firestore資料</a><br>"
     link += "<br><a href=/read_c>讀取Firestore資料(根據資料關鍵字:楊)</a><br>"
     link += "<br><a href=/search1>老師姓名查詢</a><br>"
-    link += "<br><a href=/read_c>爬取子青老師本學期課程</a><br>"
+    link += "<br><a href=/spider>爬取子青老師本學期課程</a><br>"
     return link
 
 @app.route("/mis")
