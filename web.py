80e7ed09c99dfb9a987c1a74a4926f9d80c2da76
s1080823 <s1080823@gm.pu.edu.tw>
2026-05-14 11:52:22 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -54,6 +54,16 @@ def webhook():
     if (action == "rateChoice"):
         rate =  req["queryResult"]["parameters"]["rate"]
         info = "我是呂芳妤設計的機器人，您選擇的電影分級是：" + rate +"，相關電影：\n"
+        db = firestore.client()
+        collection_ref = db.collection("本週新片含分級")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
 
 
     return make_response(jsonify({"fulfillmentText": info}))
