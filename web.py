0ea0c0d06c5fe1117352b3720342fe2495edeff4
s1080823 <s1080823@gm.pu.edu.tw>
2026-05-14 11:02:30 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, make_response, jsonify
 from datetime import datetime
 
 import os
@@ -40,6 +40,17 @@ def index():
     link += "<br><a href=/weather>查詢目前天氣</a><br>"
     link += "<br><a href=/rate>本週新片進DB</a><br>"
     return link
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
 
 
 @app.route("/rate")
